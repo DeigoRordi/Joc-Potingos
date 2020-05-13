630b9f1993dcd55c20bc3da200f5ef276a469001
DeigoRordi <60349692+DeigoRordi@users.noreply.github.com>
2020-05-13 10:53:22 +0200

    Update Carrera_Blau.gd

--- Hashigo/Scripts/Carrera_Blau.gd
+++ Hashigo/Scripts/Carrera_Blau.gd
@@ -1,12 +1,9 @@
-extends Node2D
+extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocitat = Vector2.ZERO
+var gravetat = Vector2(0,10000)
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
@@ -14,6 +11,7 @@ func _ready():
 
 func _process(delta):
 	if $".."/Start.time_left == 0:
-		position.y += 30
+		velocitat.y += gravetat.y * delta
 		if Input.is_action_just_pressed("Carrera_blau"):
 			position.x += 10
+	var moviment = move_and_slide(velocitat, Vector2.UP)
