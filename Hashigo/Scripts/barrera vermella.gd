a7bf56050eb59d125d84533fe350dfbfb4328dca
Peresv <60349608+peresv@users.noreply.github.com>
2020-05-15 18:48:32 +0200

    .
    .

---
+++ Hashigo/Scripts/barrera vermella.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+const PAD_SPEED = 150
+var velocitat = Vector2(0,0)
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _process(delta):
+	velocitat.y = 0
+	if Input.is_action_pressed("izq_mover_arriba"):
+		velocitat.y = -1000
+	if Input.is_action_pressed("izq_mover_abajo"):
+		velocitat.y = 1000
+	move_and_slide(velocitat)
