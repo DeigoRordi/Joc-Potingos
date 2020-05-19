03cb6a4d9d9cc61604d3d35f9d153ceb1b2cf747
DeigoRordi <60349692+DeigoRordi@users.noreply.github.com>
2020-05-19 11:53:48 +0200

    Pong casi acabat
    Falta:
       -posar caixes de colisions al costat
       -fer els punts
       -fer concordança amb el joc

--- Hashigo/Scripts/bola.gd
+++ Hashigo/Scripts/bola.gd
@@ -1,5 +1,11 @@
 extends KinematicBody2D
+
 var velocitat_bola = Vector2.ZERO
+var rng = RandomNumberGenerator.new()
+var time = 0
+var velocitat_maxima_y = 0
+
+
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,9 +14,30 @@ var velocitat_bola = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	velocitat_bola.x = 500
+	velocitat_bola.y = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	print(velocitat_maxima_y)
+	velocitat_maxima_y = time*(50/3)
+	$".."/Temps.text = str(time)
+	move_and_slide(velocitat_bola)
+	
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("paret"):
+		velocitat_bola.y *= -1
+	elif body.is_in_group("porteria"):
+		velocitat_bola.x *= -1
+		if velocitat_bola.y > velocitat_maxima_y:
+			velocitat_bola.y = velocitat_maxima_y
+		elif velocitat_bola.y < velocitat_maxima_y:
+			var my_random_number = rng.randf_range(-500, 500)
+			velocitat_bola.y +=  my_random_number
+
+
+func _on_Timer_timeout():
+	time += 1
